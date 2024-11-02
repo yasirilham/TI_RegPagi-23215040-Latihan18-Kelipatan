db56f732c0842f41ef95b48f44a2fd116629677c
yasirilham <yasirilham308@gmail.com>
2024-11-02 21:45:03 +0700

    Add files via upload

---
+++ Latihan18_Kelipatan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TUGAS_LATIHAN_PERTEMUAN_6;
+
+/**
+ *
+ * @author ACER
+ * NAMA : M YASIR ILHAM N
+ * KELAS : PBO1
+ * NIM : 23215040
+ * DESKRIPSI PROGRAM : Membuat program kelipatan 3,5
+ */
+public class Latihan18_Kelipatan {
+
+    public static void main(String[] args) {
+        System.out.println("Kelipatan 3.5 dari 3.5 sampai 35:");
+
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
